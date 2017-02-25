65c4a0581605bdeef2747390d5ae6fb862e0d16d
jbhv12 <jbhv12@Jays-MacBook-Pro.local>
2017-02-26 00:57:16 +0530

    created login functionality
    created local sql database. change .env and database.php file according
    to your local config. (then maybe migrate and seed). check the
    functionality with test user. Will remove signup page later.

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,14 @@
+// app/controllers/HomeController.php<
+
+...
+
+public function showLogin()
+{
+    // show the form
+    return View::make('login');
+}
+
+public function doLogin()
+{
+// process the form
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Foundation\Auth\User;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +11,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+      $this->call('UserTableSeeder');
+      //   $this->call(UsersTableSeeder::class);
     }
+}
+class UserTableSeeder extends Seeder
+{
+
+public function run()
+{
+    DB::table('users')->delete();
+    User::create(array(
+        'name'     => 'test',
+      //  'username' => 'asdf',
+        'email'    => 'test@test.com',
+        'password' => Hash::make('test'),
+    ));
+}
+
 }
